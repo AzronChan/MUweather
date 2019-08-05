3b4608c6d3df4da530e127115579274d6194f454
chenzhaolong <zlong5568863@qq.com>
2019-08-06 00:43:51 +0800

    test

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,39 +1,55 @@
 //index.js
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
+    avatarUrl: "./user-unlogin.png",
     userInfo: {},
     logged: false,
     takeSession: false,
-    requestResult: ''
+    requestResult: "",
+    weather : []
   },
 
   onLoad: function() {
     if (!wx.cloud) {
       wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
+        url: "../chooseLib/chooseLib"
+      });
+      return;
     }
-    this.getWeather();
+    let _t = this;
+    wx.getLocation({
+      type: "wgs84",
+      success:function(res){
+        //TODO 加个loading 接口有时候会很慢
+        console.log(res)
+        _t.getWeather(res);
+      },
+      fail() {
+        console.log(2222);
+      },
+      complete() {
+        console.log(333);
+      }
+    });
     // 获取用户信息
     wx.getSetting({
       success: res => {
-        if (res.authSetting['scope.userInfo']) {
+        if (res.authSetting["scope.userInfo"]) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log(res);
               this.setData({
                 avatarUrl: res.userInfo.avatarUrl,
                 userInfo: res.userInfo
-              })
+              });
             }
-          })
+          });
         }
       }
-    })
+    });
   },
 
   onGetUserInfo: function(e) {
@@ -42,92 +58,93 @@ Page({
         logged: true,
         avatarUrl: e.detail.userInfo.avatarUrl,
         userInfo: e.detail.userInfo
-      })
+      });
     }
   },
 
   onGetOpenid: function() {
     // 调用云函数
     wx.cloud.callFunction({
-      name: 'login',
+      name: "login",
       data: {},
       success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
+        console.log("[云函数] [login] user openid: ", res.result.openid);
+        app.globalData.openid = res.result.openid;
         wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
+          url: "../userConsole/userConsole"
+        });
       },
       fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
+        console.error("[云函数] [login] 调用失败", err);
         wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
+          url: "../deployFunctions/deployFunctions"
+        });
       }
-    })
+    });
   },
 
-  getWeather(){
+  getWeather(res) {
     wx.request({
-      url: 'https://free-api.heweather.net/s6/weather/now?location=guangzhou&key=ca6d5e50a6eb4e8987d60237e9c8fc35',
+      url: `https://api.caiyunapp.com/v2/RIWyHlZb6K2dvWsa/${res.longitude},${
+        res.latitude
+      }/weather.json`,
       data: {
-        x: '',
-        y: ''
+        dailysteps: 15
       },
-      success(res) {
-        console.log(res.data)
+      success : (res) => {
+        this.setData({
+          weather : []
+        })
+        console.log(res);
       }
-    })
+    });
   },
 
   // 上传图片
-  doUpload: function () {
+  doUpload: function() {
     // 选择图片
     wx.chooseImage({
       count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-
+      sizeType: ["compressed"],
+      sourceType: ["album", "camera"],
+      success: function(res) {
         wx.showLoading({
-          title: '上传中',
-        })
+          title: "上传中"
+        });
 
-        const filePath = res.tempFilePaths[0]
-        
+        const filePath = res.tempFilePaths[0];
+
         // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+        const cloudPath = "my-image" + filePath.match(/\.[^.]+?$/)[0];
         wx.cloud.uploadFile({
           cloudPath,
           filePath,
           success: res => {
-            console.log('[上传文件] 成功：', res)
+            console.log("[上传文件] 成功：", res);
 
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
+            app.globalData.fileID = res.fileID;
+            app.globalData.cloudPath = cloudPath;
+            app.globalData.imagePath = filePath;
+
             wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
+              url: "../storageConsole/storageConsole"
+            });
           },
           fail: e => {
-            console.error('[上传文件] 失败：', e)
+            console.error("[上传文件] 失败：", e);
             wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
+              icon: "none",
+              title: "上传失败"
+            });
           },
           complete: () => {
-            wx.hideLoading()
+            wx.hideLoading();
           }
-        })
-
+        });
       },
       fail: e => {
-        console.error(e)
+        console.error(e);
       }
-    })
-  },
-
-})
+    });
+  }
+});
